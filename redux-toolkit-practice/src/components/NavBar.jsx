cff6899bb8795ba99af30e3c274c857938e86f48
Joseph Maramba <josephmaramba50@gmail.com>
2022-04-25 09:36:50 +0300

    useselector

---
+++ redux-toolkit-practice/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { CartIcon } from "../assets/icons";
+import { useSelector } from "react-redux";
+
+const NavBar = () => {
+  const { amount } = useSelector((store) => store.cart);
+  const { cartItems } = useSelector((store) => store.cart);
+
+  return (
+    <nav>
+      <div className="nav-center">
+        <h3>RTK Store</h3>
+        <div className="nav-container">
+          <CartIcon />
+          <div className="amount-container">
+            <p className="total-amount">{amount}</p>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
